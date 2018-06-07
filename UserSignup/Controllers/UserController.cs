c7c1f22a93f7fb311a33a7bad7cec333e8d9d93d
Earl <earljg@gmail.com>
2018-06-06 19:42:41 -0500

    return Welcome username working

---
+++ UserSignup/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using UserSignup.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace UserSignup.Controllers
+{
+    public class UserController : Controller
+    {
+        public dynamic Cheese { get; private set; }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("/User/Add") ]
+        public IActionResult Add(User user, string verify)
+        {
+            ViewBag.username = user.Username;
+
+            return View("Index");
+        }
+    }
+}
