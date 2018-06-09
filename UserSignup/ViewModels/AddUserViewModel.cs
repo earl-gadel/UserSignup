d7f39b9b88dd60e5828e29a08d2dce10cc494dcc
Earl <earljg@gmail.com>
2018-06-09 13:46:08 -0500

    started studio 9

---
+++ UserSignup/ViewModels/AddUserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserSignup.ViewModels
+{
+    public class AddUserViewModel
+    {
+        [Required]
+        [Compare("Password")]
+        public string Verify { get; set; }
+    }
+}
